5713b8b12d53fdac45bb7bcab1ccd76cca32e9d7
Corey Hedberg <chedberg@gmail.com>
2020-03-07 18:32:13 -0500

    Working on only displaying one decimal point.

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ window.onload = function() {
 
 document.getElementById("clear").addEventListener("click", clearDisplay);
 
+// Global variables
+let currentDisplay = document.getElementById("display").innerText;
+
 // Adding event listener for all number inputs
 const buttons = document.querySelectorAll(".inputs");
 for (const button of buttons) {
   button.addEventListener("click", function() {
-    showInputOnDisplay(button.value);
+    // if the button value is a decimal, then
+    if (button.value === "." && currentDisplay.indexOf(".") !== -1) {
+      // check the display
+      // see if it contains a decimal point
+      // if it does contain a decimal point
+      // then do nothing
+      // return;
+    } /* otherwise */ else if (
+      button.value === "." &&
+      currentDisplay.indexOf(".") !== -1
+    ) {
+      // add a decimal point to screen
+      // else
+      showInputOnDisplay(button.value);
+    }
     // Used for debugging
+    console.log(`button.addEventListener : fired!`);
     console.log(button.value);
   });
 }
 
 function showInputOnDisplay(number) {
   // If display = "0" then clear the zero and show the numbers input
-  let currentDisplay = document.getElementById("display").innerText;
   if (currentDisplay === "0") {
     document.getElementById("display").innerText = "";
   }
