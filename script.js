ce0fc14b6edd3c2d758cabc181a9e5ffeb49efe0
Corey Hedberg <chedberg@gmail.com>
2020-03-06 19:20:11 -0500

    Display input is now working.

--- script.js
+++ script.js
@@ -7,19 +7,25 @@ document.getElementById("clear").addEventListener("click", clearDisplay);
 // Adding event listener for all number inputs
 const buttons = document.querySelectorAll(".inputs");
 for (const button of buttons) {
-  button.addEventListener("click", e => {
-    e.target.value = document.getElementById("display").innerText;
+  button.addEventListener("click", function() {
+    showInputOnDisplay(button.value);
     // Used for debugging
-    // console.log(button);
-    console.log(e.target.id);
-    console.log(e.target.button);
-    console.log(button);
+    console.log(button.value);
   });
+}
+
+function showInputOnDisplay(number) {
+  // If display = "0" then clear the zero and show the numbers input
+  let currentDisplay = document.getElementById("display").innerText;
+  if (currentDisplay === "0") {
+    document.getElementById("display").innerText = "";
+  }
+  document.getElementById("display").innerText += number;
 }
 
 function clearDisplay() {
   document.getElementById("display").innerText = "0";
   // Used for debugging
-  console.log("clearDisplay fired");
+  console.log(`clearDisplay : Display cleared!`);
   return;
 }
