c4c154857ace5810022c3fc911d85456f114f960
Corey Hedberg <chedberg@gmail.com>
2020-03-10 19:22:33 -0400

    We have display of the input.

--- script.js
+++ script.js
@@ -5,59 +5,36 @@ window.onload = function() {
 document.getElementById("clear").addEventListener("click", clearDisplay);
 
 // Global variables
-let currentDisplay = document.getElementById("display").textContent;
+let currentDisplay = document.getElementById("display").innerText;
 
 // Adding event listener for all number inputs
 const buttons = document.querySelectorAll(".inputs");
 for (const button of buttons) {
   button.addEventListener("click", function() {
-    console.log("currentDisplay: ", currentDisplay);
-    console.log(`currentDisplay.indexOf("."): `, currentDisplay.indexOf("."));
     if (button.value === ".") {
       if (currentDisplay.indexOf(".") !== -1) {
-        console.log(
-          "!== -1 : " +
-            currentDisplay.indexOf(".") +
-            " " +
-            typeof currentDisplay.indexOf(".")
-        );
-        console.log("currentDisplay: ", currentDisplay);
         return;
       } else if (currentDisplay.indexOf(".") === -1) {
-        console.log(
-          "=== -1 : " +
-            currentDisplay.indexOf(".") +
-            " " +
-            typeof currentDisplay.indexOf(".")
-        );
-        console.log("currentDisplay: ", currentDisplay);
         showInputOnDisplay(button.value);
         return;
       }
     }
     showInputOnDisplay(button.value);
-    // Used for debugging
-    // console.log(`button.addEventListener : fired!`);
-    // console.log(button.value);
-    // console.log(currentDisplay.indexOf("."));
   });
 }
 
 function showInputOnDisplay(number) {
-  // If display = "0" then clear the zero and show the numbers input
   if (currentDisplay === "0") {
     currentDisplay = "";
   }
   currentDisplay += number;
-  console.log("showInputOnDisplay currentDisplay: ", currentDisplay);
-  // return currentDisplay;
+  document.getElementById("display").innerText = currentDisplay;
+  return;
 }
 
 function clearDisplay() {
   currentDisplay = "0";
-  // Used for debugging
-  console.log(`clearDisplay : Display cleared!`);
-  console.log("currentDisplay: ", currentDisplay);
+  document.getElementById("display").innerText = currentDisplay;
   return;
 }
 
