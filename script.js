fec294f31c28f134a85753dbcfc479cbfea885f5
Corey Hedberg <chedberg@gmail.com>
2020-03-10 19:49:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,27 +11,35 @@ let currentDisplay = document.getElementById("display").innerText;
 const buttons = document.querySelectorAll(".inputs");
 for (const button of buttons) {
   button.addEventListener("click", function() {
+    // check to see if the button clicked is a decimal poiint
     if (button.value === ".") {
+      // if the decimal has already been previously selected, then ignore the decimal point input
       if (currentDisplay.indexOf(".") !== -1) {
         return;
       } else if (currentDisplay.indexOf(".") === -1) {
+        // if the decimal point has not been previously selected, then add a decimal point on the screen
         showInputOnDisplay(button.value);
         return;
       }
     }
+    // add the number selected on the screen
     showInputOnDisplay(button.value);
   });
 }
 
+// function to show the numbers and decimal point selected on the calculator
 function showInputOnDisplay(number) {
   if (currentDisplay === "0") {
     currentDisplay = "";
   }
+  // add the inputs together to display on the screen
   currentDisplay += number;
+  // display the inputs
   document.getElementById("display").innerText = currentDisplay;
   return;
 }
 
+// function to reset the display to "0"
 function clearDisplay() {
   currentDisplay = "0";
   document.getElementById("display").innerText = currentDisplay;
